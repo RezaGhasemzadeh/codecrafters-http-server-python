4480d70c4301be96332052537634e47e17825f40
Rez-programmer <rezacodm.reza8230@gmail.com>
2025-02-25 14:09:47 +0330

    nothing

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
     conn, _addrs = server_socket.accept() 
     if conn:
-        conn.sendall(b"HTTP/1.1 200 OK\r\n")
+        conn.sendall("HTTP/1.1 200 OK\r\n")
 
 
 if __name__ == "__main__":
