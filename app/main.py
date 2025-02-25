0058459b6d76e000c244a0482144bc1c1f1ae4f7
Rez-programmer <rezacodm.reza8230@gmail.com>
2025-02-25 18:42:50 +0330

    implemented echo endpoint

--- app/main.py
+++ app/main.py
@@ -1,16 +1,23 @@
 import socket  # noqa: F401
 
 
+def echo_endpoint(path: str):
+    data = path.split("/")[-1]
+    return data
+
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
     conn, _addrs = server_socket.accept() 
     if conn:
-        data = conn.recv(1024)
+        data = conn.recv(4096)
         request_line = data.decode().split("\r\n")[0]
         path = request_line.split(" ")[1]
         if path == "/":
             conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        elif path.startswith("/echo"):
+            result = echo_endpoint(path)
+            conn.sendall(result.encode())
         else:
             conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
