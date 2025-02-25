f3475a30676b7956c4007465a1dfcaece8d608b3
Rez-programmer <rezacodm.reza8230@gmail.com>
2025-02-25 19:30:51 +0330

    resolved some bugs

--- app/main.py
+++ app/main.py
@@ -13,10 +13,9 @@ def user_agent_endpoint(request):
     for h in headers:
         if h:
             splitted_header = h.split(":")
-            header_key = h[0]
+            header_key = splitted_header[0]
             if header_key == "User-Agent":
                 value = splitted_header[1]
-                print(f"Header Key: {header_key} || value: {value}")
     response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}".encode()
     return response
     
